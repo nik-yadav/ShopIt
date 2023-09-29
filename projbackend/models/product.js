8b4e21835a7c52ed322fcd5d8463c529b11376a0
divesh <diveshsisodiads@gmail.com>
2023-09-29 22:38:32 +0530

    Add: fields -> brand, mrp, reviews

--- projbackend/models/product.js
+++ projbackend/models/product.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema;
 
 const productSchema = new mongoose.Schema({
+    brand: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32
+    },
     name: {
         type: String,
         trim: true,
@@ -15,6 +21,18 @@ const productSchema = new mongoose.Schema({
         maxlength: 2000
     },
     price: {
+        type: Number,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    mrp: {
+        type: Number,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    reviews: {
         type: Number,
         required: true,
         maxlength: 32,
@@ -36,6 +54,6 @@ const productSchema = new mongoose.Schema({
         data: Buffer,
         contentType: String
     }
-}, {timestamps: true})
+}, { timestamps: true })
 
 module.exports = mongoose.model('Product', productSchema);
