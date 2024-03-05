2a0140777e24b819e463b08e9fabf38b7a3a61b6
divesh <diveshsisodiads@gmail.com>
2024-03-06 00:45:13 +0530

    fix: stick footer to bottom using flexbox

--- projfrontend/src/core/Layout.js
+++ projfrontend/src/core/Layout.js
@@ -7,13 +7,13 @@ function Layout() {
   const [open, setOpen] = useState(false);
 
   return (
-    <>
+    <div className="flex flex-col h-screen justify-between">
       <Navbar cartState={[open, setOpen]} />
       <div className="p-4">
         <Outlet />
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
